3c9fc529ef8efd746c928956bfa8531cf1f838ee
JinYoungKim <jin04133@pusan.ac.kr>
2024-02-01 16:45:58 +0900

    init project

---
+++ src/main/java/jin04133/JYRecord/JyRecordApplication.java
@@ -0,0 +1,13 @@
+package jin04133.JYRecord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JyRecordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JyRecordApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jin04133/JYRecord/JyRecordApplicationTests.java
@@ -0,0 +1,13 @@
+package jin04133.JYRecord;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JyRecordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
